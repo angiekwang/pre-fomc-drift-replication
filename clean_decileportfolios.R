6a17011396a143aaa91ce469c9c8f995e0049198
Angie Wang <aw1407@georgetown.edu>
2026-09-19 09:18:47 -0400

    Update clean_decileportfolios.R

--- clean_decileportfolios.R
+++ clean_decileportfolios.R
@@ -1,28 +1,38 @@
 library(readr)
 library(dplyr)
+library(here)
 library(lubridate)
 
 # Import tables: portfolio_data for decile portfolios, famafrench_data for risk-free rate
-portfolio_data <- read.csv("/Users/angiewang/Desktop/FOMCDrift/data/raw/Portfolios_Formed_on_ME_daily.csv")
-famafrench_data <- read.csv("/Users/angiewang/Desktop/FOMCDrift/data/raw/F-F_Research_Data_Factors_daily.CSV")
+portfolio_data  <- read.csv(here("Desktop", "fomc-drift", "data", "raw", "Portfolios_Formed_on_ME_daily.csv"))
+famafrench_data <- read.csv(here("Desktop", "fomc-drift", "data", "raw", "F-F_Research_Data_Factors_daily.CSV"))
 
 # Drop irrelevant columns in both dataframes
-portfolio_data <- subset(portfolio_data, select = c(Date, Lo.10, X2.Dec, X3.Dec, X4.Dec, X5.Dec, X6.Dec, X7.Dec, X8.Dec, X9.Dec, Hi.10))
-portfolio_data <- portfolio_data[c(1:25901),]
+portfolio_data  <- subset(portfolio_data, select = c(Date, Lo.10, X2.Dec, X3.Dec, X4.Dec, X5.Dec, X6.Dec, X7.Dec, X8.Dec, X9.Dec, Hi.10))
 famafrench_data <- subset(famafrench_data, select = c(Date, RF))
 
 # Merge dataframes by date
 decile_returns <- merge(portfolio_data, famafrench_data, by = "Date")
 decile_returns <- decile_returns %>% mutate(decile_returns, across(c(Lo.10, X2.Dec, X3.Dec, X4.Dec, X5.Dec, X6.Dec, X7.Dec, X8.Dec, X9.Dec, Hi.10), as.numeric))
 
-# Clean rows to only show dates between 1994-09-01 and 2011-03-31
-decile_returns$Date <- as.Date(decile_returns$Date, format = "%Y%m%d")
-decile_returns <- decile_returns[-c(1:3),]
-decile_returns <- decile_returns %>% filter(Date >= "1994-09-01" & Date <="2011-03-30")
-decile_returns <- decile_returns %>% mutate(year = year(Date))
+# Clean rows to only show dates between 1994-09-01 and 2011-03-30
 
-# Add dummy variable column: 1 if fomc announcement date, 0 otherwise
-fomc_dates <- c("19940927", "19941115", "19941220",
+decile_returns$Date <- as.Date( # Convert to date format
+  as.character(decile_returns$Date),
+  format = "%Y%m%d"
+  )
+
+decile_returns <- decile_returns %>% filter( # Filter to sample period
+  Date >= "1994-09-01" & Date <="2011-03-30"
+  )
+
+decile_returns <- decile_returns %>% mutate( # Create year variable
+  year = year(Date)
+  )
+
+# Add 'fomc' dummy variable: 1 if fomc announcement date, 0 otherwise
+
+fomc_dates <- c("19940927", "19941115", "19941220", # List of FOMC dates obtained from Fed website
                 "19950201", "19950328", "19950523", "19950706","19950822", "19950926", "19951115", "19951219",
                 "19960131", "19960521", "19960703", "19960820","19960924", "19961113", "19961217",
                 "19970205", "19970325", "19970520", "19970702", "19970819", "19970930", "19971112", "19971216",
@@ -40,8 +50,14 @@ fomc_dates <- c("19940927", "19941115", "19941220",
                 "20090128", "20090318", "20090429", "20090624", "20090812", "20090923", "20091104", "20091216",
                 "20100127", "20100316", "20100428", "20100623", "20100810", "20100921", "20101103", "20101214",
                 "20110126", "20110315")
-fomc_dates <- as.Date(fomc_dates, format = "%Y%m%d")
-decile_returns <- decile_returns %>% mutate(fomc = ifelse(Date %in% fomc_dates, 1, 0))
+
+fomc_dates <- as.Date(fomc_dates, format = "%Y%m%d") 
+
+decile_returns <- decile_returns %>% mutate( # Create dummy variable for FOMC announcement date
+  fomc = ifelse(
+    Date %in% fomc_dates, 1, 0
+    )
+  )
 
 # Add columns for excess returns and log excess returns
 decile_returns <- decile_returns %>%
@@ -87,8 +103,16 @@ decile_returns <- decile_returns %>%
       100 * (log1p(Hi.10 / 100) - log1p(RF / 100))
   )
 
-# Save decile_returns dataframe as RDS file
-saveRDS(
+# Save decile_returns dataframe as .rds file
+saveRDS(decile_returns, "/Users/angiewang/Desktop/fomc-drift/data/processed/decile_returns_clean.rds")
+
+# Save decile_returns dataframe as .csv file (without row numbers)
+write.csv(decile_returns,
+          "/Users/angiewang/Desktop/fomc-drift/data/processed/decilereturns.csv",
+          row.names = FALSE)
+
+# Save as .dta file
+haven::write_dta(
   decile_returns,
-  "/Users/angiewang/Desktop/FOMCDrift/data/processed/decile_returns_clean.rds"
+  "/Users/angiewang/Desktop/fomc-drift/data/processed/decilereturns.dta"
 )
